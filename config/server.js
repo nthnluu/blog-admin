951af85382852819c0680255c46598d192213af4
nathanluu <nate1299@me.com>
2021-02-03 20:09:15 -0500

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'aa9a1512f91aa0031adebb64f44dd20c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f2cd8134-f2ba-4e8f-accf-a025caf71e33'
+};
